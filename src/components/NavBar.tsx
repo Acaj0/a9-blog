b7bd8dd7051ebfc6848088c1ba06a00555b2fa17
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-10-15 19:53:27 -0400

    Correcao Navbar Sections

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -45,7 +45,7 @@ export default function NavBar() {
           </a>
           <a
             className="transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100 hover:text-gray-200"
-            href="#2"
+            href="/#2"
           >
             Serviços
           </a>
@@ -101,9 +101,7 @@ const MobileNav = () => {
         <div className="mt-10 flex flex-col gap-6 text-base font-semibold">
           <a href="/">Inicio</a>
           <a href="/">Sobre Nós</a>
-          <a href="Portifolio de Servicos.pdf" target="_blank">
-            Serviços
-          </a>
+          <a href="/#2">Serviços</a>
           <a href="/">Contato</a>
           <a href="/posts">Canal de Notícias</a>
         </div>
